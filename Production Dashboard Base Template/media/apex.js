c305768a6591b0b2820acb6d559b1c4bc4b2725d
Nachmani, Jacob <jacob.nachmani@intel.com>
2023-04-30 11:25:24 +0300

    Revert "updated LFU report now has working filters that update charts in real time some minor css changes done as well"
    This reverts commit 6437f9ffc9c2b3abe66d2b804472c948c9e865e0.

--- Production Dashboard Base Template/media/apex.js
+++ Production Dashboard Base Template/media/apex.js
@@ -1,251 +1,141 @@
-
-
-var table_data = [
-  ['ASML', 'Wet', '202313', 'F28', 0.794, 2308.36],
-  ['ASML', 'Wet', '202313', 'F32', 0.688, 2056.85],
-  ['ASML', 'Wet', '202314', 'F28', 0.795, 2381.41],
-  ['ASML', 'Wet', '202314', 'F32', 0.698, 1757.46],
-  ['ASML', 'Wet', '202315', 'F28', 0.799, 2210.2],
-  ['ASML', 'Wet', '202315', 'F32', 0.694, 1992.65],
-  ['ASML', 'Wet', '202316', 'F28', 0.824, 1877.85],
-  ['ASML', 'Wet', '202316', 'F32', 0.689, 2272.61],
-  ['ASML', 'Wet', '202317', 'F28', 0.835, 1641.22],
-  ['ASML', 'Wet', '202317', 'F32', 0.688, 1915.91]
-]
-
-
-
-
-
-function update_chart(series_x, series_y){
-
-  options = {
-    series: [
-    {
-      name: "F28",
-      data: series_y[0]
-    },
-
-    {
-      name: "F42",
-      data: series_y[1]
-    },
-
-    {
-      name: "RA3",
-      data: series_y[2]
-    }
-  ],
-  // x-axis should be identical
-  xaxis: {
-    categories: series_x[0],
-    title: {
-      text: 'WW'
-    }
-  },
-
-}
-
-//chart.updateOptions(options,false,false,false)
-}
-
-
-
-
-// function to get data from filters
-function filter_Data(data_array, site_value, entity_value, type_value) {
-
-  // Initlize arrays
-  var filtered_data_F28 = [];
-  var filtered_data_F32 = [];
-  var filtered_data_RA3 = [];
-
-    // if null set deafult value
-  if (site_value == null){
-      site_value = 'All_Sites';
-    }
-
-  // if null set deafult value
-  if (entity_value == null){
-      entity_value = 'ASML';
-    }
-  
-  // if null set deafult value
-  if (type_value == null){
-      type_value = 'Wet';
-    }
-
-
-  // loop thru data and appened to array - we allow to display multiple sidtes at once therefore each site has there own array
-  for (var i=0; i < data_array.length;++i){
-
-      if ((data_array[i][0] == entity_value) && (data_array[i][1] == type_value) && (data_array[i][3] == "F28") && site_value == 'F28'){
-
-            filtered_data_F28.push(data_array[i])  
-      }
-
-      if ((data_array[i][0] == entity_value) && (data_array[i][1] == type_value) && (data_array[i][3] == "F32") && (site_value == 'F42')){
-
-            filtered_data_F32.push(data_array[i])  
-      }
-
-      if ((data_array[i][0] == entity_value) && (data_array[i][1] == type_value) && (data_array[i][3] == "RA3") && (site_value == 'RA3')){
-
-            filtered_data_RA3.push(data_array[i])  
-      }
-
-      if ((data_array[i][0] == entity_value) && (data_array[i][1] == type_value) && (site_value == "All_Sites" )){
-        
-      
-        if (data_array[i][3] == 'F28'){
-          filtered_data_F28.push(data_array[i])  
+   var options = {
+          series: [{
+          name: 'Net Profit',
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+        }, {
+          name: 'Revenue',
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        }, {
+          name: 'Free Cash Flow',
+          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+        }],
+          chart: {
+          type: 'bar',
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded'
+          },
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        },
+        yaxis: {
+          title: {
+            text: '$ (thousands)'
+          }
+        },
+        fill: {
+          opacity: 1
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return "$ " + val + " thousands"
+            }
+          }
         }
+        };
 
-        if (data_array[i][3] == 'F32') {
-          filtered_data_F32.push(data_array[i])  
-        }
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+		
 
-        if (data_array[i][3] == 'RA3') {
-          filtered_data_RA3.push(data_array[i])  
-        }
-      }
-
-
-      }  
-      
-      return [filtered_data_F28 , filtered_data_F32, filtered_data_RA3]
+var options = {
+  series: [{
+  name: 'Sales',
+  data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5]
+}],
+  chart: {
+  height: 350,
+  type: 'line',
+},
+forecastDataPoints: {
+  count: 7
+},
+stroke: {
+  width: 5,
+  curve: 'smooth'
+},
+xaxis: {
+  type: 'datetime',
+  categories: ['1/11/2000', '2/11/2000', '3/11/2000', '4/11/2000', '5/11/2000', '6/11/2000', '7/11/2000', '8/11/2000', '9/11/2000', '10/11/2000', '11/11/2000', '12/11/2000', '1/11/2001', '2/11/2001', '3/11/2001','4/11/2001' ,'5/11/2001' ,'6/11/2001'],
+  tickAmount: 10,
+  labels: {
+    formatter: function(value, timestamp, opts) {
+      return opts.dateFormatter(new Date(timestamp), 'dd MMM')
+    }
   }
-
-
-function Get_filter_values()
- {
-
-  var f28_data_x = [];
-  var f28_data_y = [];
-  var f32_data_x = [];
-  var f32_data_y = [];
-  var ra3_data_x = [];
-  var ra3_data_y = [];
-
-
-  var filtered_data_array;
-
-  // get filter values 
-  const site = document.querySelector("#site-filter");
-  const entity = document.querySelector("#entity-filter");
-  const type = document.querySelector("#type-filter");
-
-  // sore filter values in local storage 
-  localStorage.setItem("site_filter", site.value)
-  localStorage.setItem("entity_filter", entity.value)
-  localStorage.setItem("type_filter", type.value)
-
-  // filter full data set  - based on locale storage values, if first time deafult values will be set 
-  filtered_data_array = filter_Data(table_data, site.value, entity.value, type.value)
-
-  // create arrays to be sent to chart --- will transposing table be faster ??? 
-  // sites that have no data will be slipped due to length 0 
-  for (var i=0; i < filtered_data_array[0].length;++i){
-    f28_data_x.push(filtered_data_array[0][i][2]) 
-    f28_data_y.push(filtered_data_array[0][i][4])   
+},
+title: {
+  text: 'Forecast',
+  align: 'left',
+  style: {
+    fontSize: "16px",
+    color: '#666'
   }
-
-  for (var i=0; i < filtered_data_array[1].length;++i){
-    f32_data_x.push(filtered_data_array[1][i][2]) 
-    f32_data_y.push(filtered_data_array[1][i][4])  
-  }
-
-  for (var i=0; i < filtered_data_array[1].length;++i){
-    ra3_data_x.push(filtered_data_array[1][i][2]) 
-    ra3_data_y.push(filtered_data_array[1][i][4])  
-  }
-
-
-
-  series_x = [f28_data_x, f32_data_x, ra3_data_x] 
-  series_y = [f28_data_y, f32_data_y, ra3_data_y]
-
-  //pdate_chart(series_x, series_y,)
-
+},
+fill: {
+  type: 'gradient',
+  gradient: {
+    shade: 'dark',
+    gradientToColors: [ '#FDD835'],
+    shadeIntensity: 1,
+    type: 'horizontal',
+    opacityFrom: 1,
+    opacityTo: 1,
+    stops: [0, 100, 100, 100]
+  },
+},
+yaxis: {
+  min: -10,
+  max: 40
 }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart2"), options);
+chart.render();
 
 
-
-
-
-function render_charts(table_data_x, table_data_y ) {
-
-
-  var options = {
-    series: [
-    {
-      name: "F28",
-      data: table_data_y[0]
-    },
-
-    {
-      name: "F42",
-      data: table_data_y[1]
-    }
-  ],
-    chart: {
-    height: 350,
-    type: 'line',
-    dropShadow: {
-      enabled: true,
-      color: '#000',
-      top: 18,
-      left: 7,
-      blur: 10,
-      opacity: 0.2
-    },
-    toolbar: {
-      show: false
-    }
+var options = {
+  series: [{
+  name: 'series1',
+  data: [31, 40, 28, 51, 42, 109, 100]
+}, {
+  name: 'series2',
+  data: [11, 32, 45, 32, 34, 52, 41]
+}],
+  chart: {
+  height: 350,
+  type: 'area'
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  curve: 'smooth'
+},
+xaxis: {
+  type: 'datetime',
+  categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+},
+tooltip: {
+  x: {
+    format: 'dd/MM/yy HH:mm'
   },
-  colors: ['#77B6EA', '#545454'],
-  dataLabels: {
-    enabled: true,
-  },
-  stroke: {
-    curve: 'smooth'
-  },
-  title: {
-    text: 'LFU Overview',
-    align: 'center'
-  },
-  grid: {
-    borderColor: '#e7e7e7',
-    row: {
-      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-      opacity: 0.5
-    },
-  },
-  markers: {
-    size: 1
-  },
-  xaxis: {
-    categories: table_data_x[0],
-    title: {
-      text: 'WW'
-    }
-  },
-  yaxis: {
-    title: {
-      text: 'Weighted LFU'
-    },
-    //min: 0.6,
-    //max: 0.9
-  },
-  legend: {
-    position: 'top',
-    horizontalAlign: 'right',
-    floating: true,
-    offsetY: -25,
-    offsetX: -5
-  }
-  };
+},
+};
 
-  chart= new ApexCharts(document.querySelector("#chart"), options);
-  chart.render();
-}
+var chart = new ApexCharts(document.querySelector("#chart3"), options);
+chart.render();
--- reg_test/views.py
+++ reg_test/views.py
@@ -1,7 +1,7 @@
 '''
-jay test.
+Reporting for Utilization Charts.
 '''
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
 from . import blueprint
 import logging
 from lrs_app import getLrsLogger
@@ -9,7 +9,6 @@ import json as JSON
 
 import os
 
-sub_menu_apps = ['Fleet_Overview', 'Entity_Breakdown', 'Entity_Statistics' , 'Weekly_Breakdown', 'Wafer_Swap_Time'] 
 
 blueprint = Blueprint('reg_test',__name__, template_folder='templates', static_folder='static')
 lgr = getLrsLogger(loggerName=__name__, level=logging.DEBUG)
@@ -19,86 +18,5 @@ hide_in_index = False
 
 @blueprint.route('/')
 def regLandingPage():
+
     return render_template('basic.html')
-
-
-@blueprint.route('/LFU_Report', methods=["POST","GET"])
-def LFU_Report():
-
-    header = ['WW',	'FAB',	'WEIGHTED_LFU',	'OUTS']
-
-    table = [
-                ['ASML', 'Wet', '202313', 'F28', 0.794, 2308.36],
-                ['ASML', 'Wet', '202313', 'F32', 0.688, 2056.85],
-                ['ASML', 'Wet', '202314', 'F28', 0.795, 2381.41],
-                ['ASML', 'Wet', '202314', 'F32', 0.698, 1757.46],
-                ['ASML', 'Wet', '202315', 'F28', 0.799, 2210.2],
-                ['ASML', 'Wet', '202315', 'F32', 0.694, 1992.65],
-                ['ASML', 'Wet', '202316', 'F28', 0.824, 1877.85],
-                ['ASML', 'Wet', '202316', 'F32', 0.689, 2272.61],
-                ['ASML', 'Wet', '202317', 'F28', 0.835, 1641.22],
-                ['ASML', 'Wet', '202317', 'F32', 0.688, 1915.91]
-            ]
-
-    #y-axsis
-    #data = [row[2] for row in table]
-
-    #x-axsis
-    #categories = [row[0] for row in table]
-    
-    #print(data)
-    #print(categories)
-
-
-    return render_template('LFU_Report.html', page_name = 'LFU_Report' , table = table)
-
-
-@blueprint.route('/Fleet_Overview')
-def Fleet_Overview():
-    # show the form, it wasn't submitted
-    return render_template('Fleet_Overview.html', page_name = 'Fleet_Overview', sub_menu_apps = sub_menu_apps)
-
-
-@blueprint.route('/Entity_Breakdown')
-def Entity_Breakdown():
-    # show the form, it wasn't submitted
-    return render_template('Entity_Breakdown.html', page_name = 'Entity_Breakdown', sub_menu_apps = sub_menu_apps)
-
-
-@blueprint.route('/Entity_Statistics')
-def Entity_Statistics():
-    # show the form, it wasn't submitted
-    return render_template('Entity_Statistics.html' , page_name = 'Entity_Statistics', sub_menu_apps = sub_menu_apps)
-
-
-@blueprint.route('/Weekly_Breakdown')
-def Weekly_Breakdown():
-    # show the form, it wasn't submitted
-    return render_template('Weekly_Breakdown.html', page_name = 'Weekly_Breakdown', sub_menu_apps = sub_menu_apps)
-
-
-@blueprint.route('/Wafer_Swap_Time')
-def Wafer_Swap_Time():
-    # show the form, it wasn't submitted
-    return render_template('Wafer_Swap_Time.html' ,page_name = 'Wafer_Swap_Time', sub_menu_apps = sub_menu_apps)
-
-
-@blueprint.route('/SAU_Tactic_Dispatch')
-def SAU_Tactic_Dispatch():
-
-    # show the form, it wasn't submitted
-    return render_template('SAU_Tactic_Dispatch.html', page_name = 'SAU_Tactic_Dispatch', sub_menu_apps = sub_menu_apps)
-
-
-@blueprint.route('/Tactic_Dispatch_193BE')
-def Tactic_Dispatch_193BE():
-
-    # show the form, it wasn't submitted
-    return render_template('193BE_Tactic_Dispatch.html' , page_name = '193BE_Tactic_Dispatch', sub_menu_apps = sub_menu_apps)
-
-
-@blueprint.route('/Reg_Tactic_Dispatch')
-def Reg_Tactic_Dispatch():
-
-    # show the form, it wasn't submitted
-    return render_template('Reg_Tactic_Dispatch.html' ,page_name = 'Reg_Tactic_Dispatch', sub_menu_apps = sub_menu_apps)
